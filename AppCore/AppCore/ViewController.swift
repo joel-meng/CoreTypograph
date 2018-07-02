d87020ce8312fd35248cd1f5a055eed6a304b95e
Joel Meng <joel.meng@suncorp.com.au>
2018-07-02 16:45:02 +1000

    Added initial app core

---
+++ AppCore/AppCore/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AppCore
+//
+//  Created by MENG, Joel on 2/7/18.
+//  Copyright © 2018 MENG, Joel. All rights reserved.
+//
+
+import UIKit
+import CoreTypography
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
