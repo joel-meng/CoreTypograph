e40dc49ab5ea87a54c599bed40a58d563881403d
Joel Meng <joel.meng@suncorp.com.au>
2018-07-02 16:45:19 +1000

    Added core typography

---
+++ CoreTypography/CoreTypography/CoreTypography/CoreTypography.h
@@ -0,0 +1,19 @@
+//
+//  CoreTypography.h
+//  CoreTypography
+//
+//  Created by MENG, Joel on 2/7/18.
+//  Copyright © 2018 MENG, Joel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreTypography.
+FOUNDATION_EXPORT double CoreTypographyVersionNumber;
+
+//! Project version string for CoreTypography.
+FOUNDATION_EXPORT const unsigned char CoreTypographyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreTypography/PublicHeader.h>
+
+
