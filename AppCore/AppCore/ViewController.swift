676371f203fc7c2607eb2c848cd1440faeec8eb7
Joel Meng <joel.meng@suncorp.com.au>
2018-07-10 13:34:48 +1000

    Added template

--- AppCore/AppCore/ViewController.swift
+++ AppCore/AppCore/ViewController.swift
@@ -11,9 +11,35 @@ import CoreTypography
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var label1: UILabel!
+    @IBOutlet private var label3: UILabel!
+    @IBOutlet private var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        var fontDescriptorAttributes: [UIFontDescriptor.AttributeName: Any] = [
+            .name: "Taviraj",
+//            .traits: [UIFontDescriptorSymbolicTraits.traitItalic]
+        ]
+        
+//        fontDescriptorAttributes[.face] = "Bold"
+        fontDescriptorAttributes[.size] = 7
+        
+        
+        var fontDescriptor = UIFontDescriptor(fontAttributes: fontDescriptorAttributes)
+        fontDescriptor = fontDescriptor.withSymbolicTraits(UIFontDescriptorSymbolicTraits.traitItalic)!
+        print(fontDescriptor)
+        let font = UIFont(descriptor: fontDescriptor, size: 15)
+        
+        
+        label1.font = font
+        label1.text = "Hello Taviraj"
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,3 +48,60 @@ class ViewController: UIViewController {
     }
 }
 
+protocol FontDescrbing {
+    
+    var fontName: String { get }
+    
+    func fontFace(forTextStyle textStyle: UIFontTextStyle) -> String
+    
+    func fontTraits(forTextStyle textStyle: UIFontTextStyle) -> UIFontDescriptorSymbolicTraits?
+}
+
+protocol ScalableFontDescribing: FontDescrbing {
+    
+    func fontPointSize(forTextStyle textStyle: UIFontTextStyle, contentSizeCategory: UIContentSizeCategory) -> CGFloat
+}
+
+struct Taviraj: ScalableFontDescribing {
+    
+    var fontName: String { return "Taviraj" }
+    
+    func fontFace(forTextStyle textStyle: UIFontTextStyle) -> String {
+        switch textStyle {
+        case .headline:
+            return "Bold"
+        case .footnote:
+            return "Light"
+        default:
+            return "Regular"
+        }
+    }
+    
+    func fontTraits(forTextStyle textStyle: UIFontTextStyle) -> UIFontDescriptorSymbolicTraits? {
+        switch textStyle {
+        case .callout:
+            return .traitItalic
+        default:
+            return nil
+        }
+    }
+    
+    func fontPointSize(forTextStyle textStyle: UIFontTextStyle, contentSizeCategory: UIContentSizeCategory) -> CGFloat {
+        return fontSizeFactory(forTextStyle: textStyle)(contentSizeCategory)
+    }
+}
+
+typealias FontSizeFactory = (UIContentSizeCategory) -> CGFloat
+
+private func fontSizeFactory(forTextStyle textStyle: UIFontTextStyle) -> FontSizeFactory {
+    switch textStyle {
+    case .body:
+        return bodyFontSize
+    default:
+        return bodyFontSize
+    }
+}
+
+private func bodyFontSize(forContentSizeCategory contentSizeCategory:UIContentSizeCategory) -> CGFloat {
+    return 0
+}
