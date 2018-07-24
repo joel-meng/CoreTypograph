0812fed119a99f1d51a376ce5abd0fc55deac947
Joel Meng <joel.meng@suncorp.com.au>
2018-07-24 14:46:27 +1000

    added small caps playground

---
+++ FontFeaturePlayground.playground/Pages/LetterCase.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+
+
+import CoreTypography
+import UIKit
+import PlaygroundSupport
+
+//: #####
+
+let smallCapsTitle = "Small caps"
+let smallCapsText = "Text that should be small caps"
+let smallCapsFontName = "Baskerville"
+
+let smallCapsOn = FontBuilder { builder in
+    builder.lowerCase(LowerCase.smallCaps).name(smallCapsFontName)
+}.build()
+
+let smallCapsOff = FontBuilder { builder in
+    builder.name(smallCapsFontName).name(smallCapsFontName)
+}.build()
+
+let smallCapsView = textComparisonView(text: smallCapsText, font1: smallCapsOn, font2: smallCapsOff, title: smallCapsTitle)
+
+
+//: [Next](@next)
